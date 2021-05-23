4bfd23ade5e1e56d72693eb4fe4307dcaee51cf1
Bogdan <bogdan.oloeriu@gmail.com>
2021-05-23 12:08:33 +0200

    added sign out

--- CrowdfundingNative/src/components/Home.js
+++ CrowdfundingNative/src/components/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {View, Text, Button} from 'react-native';
 import {ScreenContainer} from './ScreenContainer';
+import {AuthContext } from '../utils/context'
 
 export const Home = ({navigation}) => {
+  const {signOut} = React.useContext(AuthContext)
   return (
     <ScreenContainer>
       <Text>Hello Home</Text>
       <Button title="Send Funds" onPress={() => navigation.push("SendFunds")}/>
+      <Button title="Sign out" onPress={() => signOut()}/>
     </ScreenContainer>
   );
 };
