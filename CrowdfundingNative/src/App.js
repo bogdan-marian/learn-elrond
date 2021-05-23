c12a2dc8991b6538dc2a93df9cb9387a5d960422
Bogdan <bogdan.oloeriu@gmail.com>
2021-05-23 10:59:44 +0200

    added HomeStack

--- CrowdfundingNative/src/App.js
+++ CrowdfundingNative/src/App.js
@@ -9,20 +9,36 @@ import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import {Text, View} from 'react-native';
 import {SignIn} from './components/SignIn';
 import {Home} from './components/Home';
+import {About} from './components/About';
 
 const AuthStack = createStackNavigator();
+const Tabs = createBottomTabNavigator();
+const HomeStack = createStackNavigator();
+
+const HomeStackScreen = () => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={Home} />
+    </HomeStack.Navigator>
+  );
+};
 
 const App = () => {
   return (
     <NavigationContainer>
-      <AuthStack.Navigator>
+      <Tabs.Navigator>
+        <Tabs.Screen name="Home" component={HomeStackScreen} />
+        <Tabs.Screen name="About" component={About} />
+      </Tabs.Navigator>
+      {/* <AuthStack.Navigator>
         <AuthStack.Screen name="SignIn" component={SignIn} />
         <AuthStack.Screen name="Home" component={Home} />
-      </AuthStack.Navigator>
+      </AuthStack.Navigator> */}
     </NavigationContainer>
   );
 };
