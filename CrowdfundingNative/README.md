969efb573ae5f52af2d9b5579316a880aff91c74
Bogdan <bogdan.oloeriu@gmail.com>
2021-05-23 10:20:26 +0200

    Cleaned the App

---
+++ CrowdfundingNative/README.md
@@ -0,0 +1,6 @@
+# just notes
+
+```bash
+npx react-native start
+npx react-native run-android
+```
---
+++ CrowdfundingNative/src/App.js
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  
+
+  return (
+    <View >
+      <Text>
+        Hello react native
+      </Text>
+    </View>
+  );
+};
+
+
+export default App;
