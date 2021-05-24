b2dac17999cb23d9214970135c11750d26f7186f
Bogdan <bogdan.oloeriu@gmail.com>
2021-05-24 11:05:29 +0200

    added wallet allso to home

--- CrowdfundingNative/src/components/Home.js
+++ CrowdfundingNative/src/components/Home.js
@@ -4,10 +4,14 @@ import {ScreenContainer} from './ScreenContainer';
 import {AuthContext } from '../utils/standardcontext'
 
 export const Home = ({navigation}) => {
-  const {signOut} = React.useContext(AuthContext)
+  const {signOut, getKeyFile} = React.useContext(AuthContext)
   return (
     <ScreenContainer>
       <Text>Hello Home</Text>
+
+      <Text>-------------------------------</Text>
+      <Text>Wallet: {getKeyFile().address}</Text>
+      <Text>-------------------------------</Text>
       <Button title="Send Funds" onPress={() => navigation.push("SendFunds")}/>
       <Button title="Sign out" onPress={() => signOut()}/>
     </ScreenContainer>
