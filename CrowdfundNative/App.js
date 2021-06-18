47f357d4cd2764cd8e4461d0f674c932b510f663
Bogdan <bogdan.oloeriu@gmail.com>
2021-06-18 20:19:36 +0200

    created simple app

---
+++ CrowdfundNative/App.js
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ CrowdfundNative/README.md
@@ -0,0 +1,7 @@
+Current contract address: `erd1qqqqqqqqqqqqqpgq6dxyslcylycskvxx9grm8w0t9nhfqfxuu4cqcje5kd`
+
+```bash
+npx react-native init CrowdfundNative
+yarn react-native start
+yarn react-native run-android
+```
