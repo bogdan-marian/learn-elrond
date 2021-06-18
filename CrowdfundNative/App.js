c2a8d5f3c7e0dcd5b7a3429a6dae519aea6f96f4
Bogdan <bogdan.oloeriu@gmail.com>
2021-06-18 21:33:08 +0200

    added bottom tabs

--- CrowdfundNative/App.js
+++ CrowdfundNative/App.js
@@ -2,18 +2,27 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {SignIn} from './components/SignIn';
 import {CreateAccount} from './components/CreateAccount';
+import {Profile} from './components/Profile';
+import {Home} from './components/Home';
 
 const AuthStack = createStackNavigator();
+const Tabs = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <AuthStack.Navigator>
+      <Tabs.Navigator>
+        <Tabs.Screen name="Home" component={Home} />
+        <Tabs.Screen name="Profile" component={Profile} />
+      </Tabs.Navigator>
+
+      {/* <AuthStack.Navigator>
         <AuthStack.Screen name="SignIn" component={SignIn} />
         <AuthStack.Screen name="CreateAccount" component={CreateAccount} />
-      </AuthStack.Navigator>
+      </AuthStack.Navigator> */}
     </NavigationContainer>
   );
 };
--- CrowdfundNative/components/Profile.js
+++ CrowdfundNative/components/Profile.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
-import { ScreenContainer } from "./ScreenContainer";
-import { AuthContext } from "../context";
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+import {ScreenContainer} from './ScreenContainer';
+import {AuthContext} from '../utils/context';
 
-export const Profile = ({ navigation }) => {
-  const { signOut } = React.useContext(AuthContext);
+export const Profile = ({navigation}) => {
+  //const {signOut} = React.useContext(AuthContext);
   return (
     <ScreenContainer>
       <Text>Prifile Screen</Text>
       <Button title="Drawer" onPress={() => navigation.toggleDrawer()} />
-      <Button title="Sign Out" onPress={() => signOut()} />
+      <Button title="Sign Out" onPress={() => alert('Todo')} />
     </ScreenContainer>
   );
 };
