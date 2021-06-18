5007cdcdbd862eeddae8fd46a55684d00ec8b1b0
Bogdan <bogdan.oloeriu@gmail.com>
2021-06-18 21:42:46 +0200

    pauza

--- CrowdfundNative/App.js
+++ CrowdfundNative/App.js
@@ -7,16 +7,39 @@ import {SignIn} from './components/SignIn';
 import {CreateAccount} from './components/CreateAccount';
 import {Profile} from './components/Profile';
 import {Home} from './components/Home';
+import {Search} from './components/Search';
+import {Search2} from './components/Search2';
+import {Details} from './components/Details';
 
 const AuthStack = createStackNavigator();
 const Tabs = createBottomTabNavigator();
+const HomeStack = createStackNavigator();
+const SearchStack = createStackNavigator();
+
+const HomeStackScreen = () => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={Home} />
+      <HomeStack.Screen name="Details" component={Details} />
+    </HomeStack.Navigator>
+  );
+};
+
+const SearchStackScreen = () => {
+  return (
+    <SearchStack.Navigator>
+      <SearchStack.Screen name="Search" component={Search} />
+      <SearchStack.Screen name="Search2" component={Search2} />
+    </SearchStack.Navigator>
+  );
+};
 
 const App = () => {
   return (
     <NavigationContainer>
       <Tabs.Navigator>
-        <Tabs.Screen name="Home" component={Home} />
-        <Tabs.Screen name="Profile" component={Profile} />
+        <Tabs.Screen name="Home" component={HomeStackScreen} />
+        <Tabs.Screen name="Search" component={SearchStackScreen} />
       </Tabs.Navigator>
 
       {/* <AuthStack.Navigator>
--- CrowdfundNative/components/Search.js
+++ CrowdfundNative/components/Search.js
@@ -1,18 +1,21 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
-import {ScreenContainer} from "./ScreenContainer"
-import {Search2} from "./Search2"
+import React from 'react';
+import {Text, Button} from 'react-native';
+import {ScreenContainer} from './ScreenContainer';
 
-export const Search = ({ navigation }) => {
+export const Search = ({navigation}) => {
   return (
     <ScreenContainer>
       <Text>Search Screen</Text>
-      <Button title="Search 2" onPress={()=> navigation.push("Search2")}/>
-      <Button title="React Native School" onPress={()=> 
-      navigation.navigate('Home',{
-        screen: 'Details',
-        params:{name: "React Native School navigate"}
-      })}/>
+      <Button title="Search 2" onPress={() => navigation.push('Search2')} />
+      <Button
+        title="React Native School"
+        onPress={() =>
+          navigation.navigate('Home', {
+            screen: 'Details',
+            params: {name: 'React Native School navigate'},
+          })
+        }
+      />
     </ScreenContainer>
   );
 };
