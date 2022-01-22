ad701986fb551704819d16a6b51d87220570eed5
Bogdan <bogdan.oloeriu@gmail.com>
2022-01-22 23:28:04 +0100

    step 5 address owner

--- my-contract/src/my_contract.rs
+++ my-contract/src/my_contract.rs
@@ -4,9 +4,13 @@ elrond_wasm::imports!();
 
 #[elrond_wasm::derive::contract]
 pub trait MyContract {
-
     #[init]
     fn init(&self) {
+        let _my_address: ManagedAddress = self.blockchain().get_caller();
+        self.set_owner(&_my_address);
     }
 
+    // <storage section>
+    #[storage_set("owner")]
+    fn set_owner(&self, address: &ManagedAddress);
 }
