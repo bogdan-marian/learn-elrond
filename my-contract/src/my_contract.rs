770db45a7ed9697ee9ac0da879d1db55586720c3
Bogdan <bogdan.oloeriu@gmail.com>
2022-01-22 23:17:24 +0100

    Revert "added storage"
    This reverts commit d189054cb38b4e5f2fbe10517cd21784249affaa.

--- my-contract/src/my_contract.rs
+++ my-contract/src/my_contract.rs
@@ -7,11 +7,6 @@ pub trait MyContract {
 
     #[init]
     fn init(&self) {
-        let _my_address: ManagedAddress = self.blockchain().get_caller();
-        self.set_owner(&_my_address);
     }
-
-    #[storage_set("owner")]
-    fn set_owner(&self, address: &ManagedAddress);
 
 }
