5b19e3d2df1c36a207aabbdc759a1f2897d19b2b
Bogdan <bogdan.oloeriu@gmail.com>
2022-02-05 23:07:20 +0100

    added issue_token

--- my-contract/src/nft_module.rs
+++ my-contract/src/nft_module.rs
@@ -124,6 +124,56 @@ pub trait NftModule{
         Ok(())
     }
 
+
+    #[only_owner]
+    #[payable("EGLD")]
+    #[endpoint(issueToken)]
+    fn issue_token(
+        &self,
+        #[payment] issue_cost: BigUint,
+        token_name: ManagedBuffer,
+        token_ticker: ManagedBuffer,
+    ) -> SCResult<AsyncCall> {
+        require!(self.nft_token_id().is_empty(), "Token already issued");
+
+        Ok(self
+            .send()
+            .esdt_system_sc_proxy()
+            .issue_non_fungible(
+                issue_cost,
+                &token_name,
+                &token_ticker,
+                NonFungibleTokenProperties {
+                    can_freeze: true,
+                    can_wipe: true,
+                    can_pause: true,
+                    can_change_owner: false,
+                    can_upgrade: false,
+                    can_add_special_roles: true,
+                },
+            )
+            .async_call()
+            .with_callback(self.callbacks().issue_callback()))
+    }
+
+    #[callback]
+    fn issue_callback(&self, #[call_result] result: ManagedAsyncCallResult<TokenIdentifier>) {
+        match result {
+            ManagedAsyncCallResult::Ok(token_id) => {
+                self.nft_token_id().set(&token_id);
+            },
+            ManagedAsyncCallResult::Err(_) => {
+                let caller = self.blockchain().get_owner_address();
+                let (returned_tokens, token_id) = self.call_value().payment_token_pair();
+                if token_id.is_egld() && returned_tokens > 0 {
+                    self.send()
+                        .direct(&caller, &token_id, 0, &returned_tokens, &[]);
+                }
+            },
+        }
+    }
+
+
     // storage
     #[view(getTokenId)]
     #[storage_mapper("nftTokenId")]
