fcd08ad0f497c77d8a8418cb8c8bd68e345206ea
Bogdan <bogdan.oloeriu@gmail.com>
2022-02-05 23:16:29 +0100

    issueToken invalid fuction not found

--- my-contract/interaction/devnet.snippets.sh
+++ my-contract/interaction/devnet.snippets.sh
@@ -12,6 +12,15 @@ GAS_SMALL="50000000"
 DEPLOY_GAS="65000000"
 BUY_GAS="10000000"
 
+MINT_COST_VAL=$(echo '0.05*(10^18)' | bc)
+MINT_COST=$(echo $MINT_COST_VAL | awk -F "." '{print $1}')
+TOKEN_NAME="TEST001xToken"
+TOKEN_NAME_HEX=$(echo -n ${TOKEN_NAME} | xxd -p)
+TOKEN_REVERSE=$(xxd -r -p <<<"$TOKEN_NAME_HEX")
+TOKEN_TICKER="TEST001"
+TOKEN_TICKER_HEX=$(echo -n ${TOKEN_TICKER} | xxd -p)
+ISSUE_TOKEN_ARGUMENTS="0x${TOKEN_NAME_HEX} 0x${TOKEN_TICKER_HEX}"
+
 deploy() {
   erdpy --verbose contract deploy --project=${PROJECT} --recall-nonce --pem=${ALICE} \
     --gas-limit=${DEPLOY_GAS} --send --outfile="${MY_LOGS}/deploy-devnet.interaction.json" \
@@ -25,4 +34,13 @@ deploy() {
 
   echo ""
   echo "Smart contract address: ${ADDRESS}"
+}
+
+issueToken() {
+  erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=${GAS_LIMIT} \
+    --function="issueToken" \
+    --value ${MINT_COST} \
+    --arguments ${ISSUE_TOKEN_ARGUMENTS} \
+    --proxy=${PROXY} --chain=${CHAINID} --send \
+    --outfile="${MY_LOGS}/issueToken.json"
 }
