d3f9a43d0b392e5deaca49c188f94761e678dbb4
Bogdan <bogdan.oloeriu@gmail.com>
2022-02-16 20:50:01 +0100

    moved n02 over this public repo

---
+++ archive/n02-node-cloud-run/index.ts
@@ -0,0 +1,140 @@
+const express = require("express");
+
+import path = require("path");
+import fs = require("fs");
+import {
+  Account,
+  Address,
+  UserSigner,
+  SmartContractAbi,
+  Balance,
+  Code,
+  ContractFunction,
+  GasLimit,
+  NetworkConfig,
+  ProxyProvider,
+  SmartContract,
+} from "@elrondnetwork/erdjs";
+import {
+  AbiRegistry,
+  AddressValue,
+  BytesValue,
+  U32Value,
+} from "@elrondnetwork/erdjs/out/smartcontracts/typesystem";
+import { BinaryCodec } from "@elrondnetwork/erdjs/out/smartcontracts/codec";
+import BigNumber from "bignumber.js";
+import { readFileSync, accessSync, constants, writeFileSync } from "fs";
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log("Hello world listening on port", port);
+  queryCrowdFund();
+});
+
+app.get("/", (req, res) => {
+  console.log("Hello world received a request.");
+
+  const target = process.env.TARGET || "World";
+  res.send(`Hello ${target}!`);
+});
+
+app.get("/tournament/:tournamentId/", getTournament);
+
+function getTournament(request, response) {
+  let tournamentId = request.params.tournamentId;
+  console.log("tournamentId=" + tournamentId);
+
+  let reply = {
+    success: true,
+    message: "getTournament successfully called",
+    tournamentId: tournamentId,
+  };
+
+  runInteractions();
+
+  response.send(reply);
+}
+
+async function runInteractions() {
+  let codec = new BinaryCodec();
+  let provider = new ProxyProvider("http://localhost:7950");
+  let keyFilePath = path.resolve(__dirname, "wallets/users/alice.json");
+  let keyFileJson = fs.readFileSync(keyFilePath, { encoding: "utf8" });
+  let keyFileObject = JSON.parse(keyFileJson);
+  let password = "password";
+  let signer = UserSigner.fromWallet(keyFileObject, password);
+  let user = new Account(signer.getAddress());
+
+  let abiPath = path.resolve(__dirname, "abi.json");
+  let abiJson = fs.readFileSync(abiPath, { encoding: "utf8" });
+  let abiObject = JSON.parse(abiJson);
+  let abi = new AbiRegistry();
+  abi.extend(abiObject);
+  //let namespace = abi.findNamespace("lottery-egld");
+
+  console.log("End of runInteractions");
+}
+
+export const createSmartContractInstance = (
+  abi?: AbiRegistry,
+  address?: string
+) => {
+  const contract = new SmartContract({
+    address: address ? new Address(address) : undefined,
+    abi:
+      abi &&
+      new SmartContractAbi(
+        abi,
+        abi.interfaces.map((iface) => iface.name)
+      ),
+  });
+  return contract;
+};
+
+async function getAbi(filePath: string) {
+  try {
+    console.log("File path: " + filePath)
+    let myPath = path.resolve(filePath);
+    console.log(myPath)
+    let registry = await AbiRegistry.load({ files: [filePath] });
+    //return registry;
+  }catch(e){
+    console.log("We had an error AbiRegistry.load", e.message)
+  }
+}
+
+async function queryCrowdFund() {
+  const address = new Address(
+    "erd1qqqqqqqqqqqqqpgq6p6mr5hc3ksl0t6u72454hg6l7qnteyrd8sslxsa3d"
+  );
+  const proxy: ProxyProvider = new ProxyProvider(
+    "https://devnet-gateway.elrond.com"
+  );
+
+  // const contract: SmartContract = new SmartContract({ address });
+  // const func: ContractFunction = new ContractFunction("currentFunds");
+  // const qResponse = await contract.runQuery(proxy, { func });
+  // qResponse.assertSuccess();
+  // const returnData = qResponse.returnData;
+  // let values = returnData.values();
+
+  // console.log(returnData);
+  var abiFile;
+  getAbi("./e02-crowdfunding.abi.json");
+
+  let stamp = Date();
+  console.log("End of query crowdfund " + stamp);
+}
+
+async function queryChessout() {
+  let contract = new SmartContract({
+    address: new Address(
+      "erd1qqqqqqqqqqqqqpgqxwakt2g7u9atsnr03gqcgmhcv38pt7mkd94q6shuwt"
+    ),
+  });
+  let addressOfAlice = new Address(
+    "erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th"
+  );
+}
---
+++ archive/n02-node-cloud-run/readme.md
@@ -0,0 +1,98 @@
+# google docker cloud run and node
+
+## some links
+
+[node server side](https://shiffman.net/a2z/server-node/#:~:text=%E2%80%A2-,Node.,in%20a%20file%20called%20hello.)
+[google cloud run](https://codelabs.developers.google.com/codelabs/cloud-run-hello#0)
+[elrond rest](https://docs.elrond.com/sdk-and-tools/rest-api/rest-api/#proxy-api-vs-observer-api)
+[ts-node-dev](https://github.com/wclr/ts-node-dev)
+[good javascript query snippets](https://elrondnetwork.github.io/elrond-sdk-docs/erdjs/latest/#creating-value-transfer-transactions)
+[interact with the smart contract](https://www.julian.io/articles/elrond-erdjs.html)
+
+## query the blockchain
+
+```text
+Martin Wagner | CIO | Knights of Cathena, [14.02.22 21:20]
+[In reply to Bogdan Oloeriu]
+Unless you want to make more than 50 request per minute in which case you would get rate limited by the public gateway, yes :)
+In that case you would either have to use bware labs api or setup your own observer squad :)
+```
+
+## outdated ts interaction file
+
+```text
+Martin Wagner | CIO | Knights of Cathena, [14.02.22 23:31]
+Use the UserSigner to replace the BackendSigner
+https://github.com/ElrondNetwork/elrond-sdk-erdjs/blob/bdec7b5fb1335dba3311d721681f35a16f1f4f5a/src/walletcore/userSigner.ts#L18
+
+Martin Wagner | CIO | Knights of Cathena, [14.02.22 23:32]
+And Argument is a bit more tricky since it was split into several files, but there is BytesValue.fromUtf8 now and stuff like new BigUintValue() to replace it :)
+```
+
+## actual log
+
+```bash
+gcloud auth login
+GOOGLE_CLOUD_PROJECT="mysandbox-v3"
+gcloud config set project ${GOOGLE_CLOUD_PROJECT}
+gcloud config set compute/zone europe-west1-b
+gcloud services enable run.googleapis.com
+
+mkdir n02-node-cloud-run
+cd n02-node-cloud-run
+npm init
+npm install express
+
+gcloud builds submit --tag gcr.io/$GOOGLE_CLOUD_PROJECT/helloworld
+gcloud container images list
+gcloud auth configure-docker
+
+# run the container
+docker run -d -p 8080:8080 gcr.io/$GOOGLE_CLOUD_PROJECT/helloworld
+
+# deploy
+gcloud run deploy helloworld \
+  --image gcr.io/$GOOGLE_CLOUD_PROJECT/helloworld \
+  --platform managed \
+  --region europe-west1 \
+  --allow-unauthenticated
+
+# clean everything up
+gcloud container images delete gcr.io/$GOOGLE_CLOUD_PROJECT/helloworld
+
+# delete cloud run instance
+gcloud run services delete helloworld \
+  --platform managed \
+  --region europe-west1
+```
+
+# nodemon
+
+```bash
+cd n02-node-cloud-run/
+npm install --save-dev nodemon
+node_modules/nodemon/bin/nodemon.js index.js
+
+npm install --save-dev fs
+
+yarn add @elrondnetwork/erdjs
+yarn add ts-node
+yarn add typescript
+yarn add ts-node-dev --dev
+
+node_modules/ts-node-dev/lib/bin.js index.ts
+
+```
+
+# kill running processes
+
+[kill running processes](https://stackoverflow.com/questions/9346211/how-to-kill-a-process-on-a-port-on-ubuntu)
+
+```bash
+sudo kill -9 `sudo lsof -t -i:8080`
+```
+
+# run elrond tests
+```bash
+npm install mocha
+```
