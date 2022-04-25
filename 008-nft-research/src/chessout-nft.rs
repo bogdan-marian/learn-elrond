d165058082e50a8417a589947b25b3635f7c7e53
Bogdan <bogdan.oloeriu@gmail.com>
2022-04-25 20:11:26 +0200

    sha256 method not found

--- 008-nft-research/src/chessout-nft.rs
+++ 008-nft-research/src/chessout-nft.rs
@@ -91,6 +91,50 @@ pub trait ChessoutNft  {
             .call_and_exit()
     }
 
+
+    #[allow(clippy::too_many_arguments)]
+    fn create_nft_with_attributes<T: TopEncode>(
+        &self,
+        name: ManagedBuffer,
+        royalties: BigUint,
+        attributes: T,
+        uri: ManagedBuffer,
+        selling_price: BigUint,
+        token_used_as_payment: TokenIdentifier,
+        token_used_as_payment_nonce: u64,
+    ) -> u64 {
+        require!(!self.nft_token_id().is_empty(), "Token not issued");
+        require!(royalties <= ROYALTIES_MAX, "Royalties cannot exceed 100%");
+
+        let nft_token_id = self.nft_token_id().get();
+
+        let mut serialized_attributes = ManagedBuffer::new();
+        if let core::result::Result::Err(err) = attributes.top_encode(&mut serialized_attributes) {
+            sc_panic!("Attributes encode error: {}", err.message_bytes());
+        }
+
+        let attributes_sha256 = self.crypto().sha256(&serialized_attributes);
+        let attributes_hash = attributes_sha256.as_managed_buffer();
+        let uris = ManagedVec::from_single_item(uri);
+        let nft_nonce = self.send().esdt_nft_create(
+            &nft_token_id,
+            &BigUint::from(NFT_AMOUNT),
+            &name,
+            &royalties,
+            attributes_hash,
+            &attributes,
+            &uris,
+        );
+
+        self.price_tag(nft_nonce).set(&PriceTag {
+            token: token_used_as_payment,
+            nonce: token_used_as_payment_nonce,
+            amount: selling_price,
+        });
+
+        nft_nonce
+    }
+
     // storage
     #[view(getTokenId)]
     #[storage_mapper("nftTokenId")]
